06d2ce6da9b1f0b8c2742c87352cad8e585ac482
imran-moonward <imran@moonward.com.au>
2023-11-22 21:24:26 +1000

    added the whole store logic

--- src/store/notes-store.ts
+++ src/store/notes-store.ts
@@ -1,12 +1,19 @@
 import {create} from 'zustand';
-import {Note} from '../types/general-types';
+import {CategoryType, Client, Note} from '../types/general-types';
+import {categoryList, clientList} from '../constants/constants';
 
 type State = {
   notes: Note[];
+  categories: CategoryType[];
+  clientList: Client[];
 };
 
 type Action = {
   addRemoveNote: (note: Note) => void;
+  addClient: (client: Client) => void;
+  removeClient: (client: Client) => void;
+  addCategory: (categoryType: CategoryType) => void;
+  removeCategory: (categoryType: CategoryType) => void;
   updateNote: (note: Note) => void;
 };
 
@@ -14,6 +21,8 @@ type NoteStore = State & Action;
 
 const useNoteStore = create<NoteStore>(set => ({
   notes: [],
+  clientList: clientList,
+  categories: categoryList,
   addRemoveNote(note) {
     set(state => {
       const {notes} = state;
@@ -28,7 +37,46 @@ const useNoteStore = create<NoteStore>(set => ({
       return {notes: addedNotes};
     });
   },
-  updateNote(note) {},
+  addCategory(category) {
+    set(state => {
+      return {
+        categories: [...state.categories, category],
+      };
+    });
+  },
+  removeCategory(category) {
+    set(state => {
+      return {
+        categories: state.categories.filter(c => c !== category),
+      };
+    });
+  },
+  addClient(client) {
+    set(state => {
+      return {
+        clientList: [...state.clientList, client],
+      };
+    });
+  },
+  removeClient(client) {
+    set(state => {
+      return {
+        clientList: state.clientList.filter(c => c.id !== client.id),
+      };
+    });
+  },
+  updateNote(noteToUpdate) {
+    set(state => {
+      const {notes} = state;
+      const updatedNotes = notes.map(note => {
+        if (note.id === noteToUpdate.id) {
+          return {...note, ...noteToUpdate};
+        }
+        return note;
+      });
+      return {notes: updatedNotes};
+    });
+  },
 }));
 
 export default useNoteStore;
